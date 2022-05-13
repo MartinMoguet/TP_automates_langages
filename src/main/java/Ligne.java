862e4f2429584844c8799a0c40dd12d7dad5123b
PaulineBonnet1 <pauline.bonnet@etu.uphf.fr>
2022-05-13 15:32:58 +0200

    horaires dans station et pas dans ligne

--- src/main/java/Ligne.java
+++ src/main/java/Ligne.java
@@ -2,15 +2,11 @@ import java.util.ArrayList;
 
 public class Ligne {
     private String transport;
-    private String horaire_depart;
-    private String horaire_arrivee;
     private String num_ligne;
     private ArrayList <Station> listStation;
 
-    public Ligne(String transport, String horaire_depart, String horaire_arrivee, String num_ligne, ArrayList <Station> listStation) { {
+    public Ligne(String transport, String num_ligne, ArrayList <Station> listStation) { {
         this.transport = transport;
-        this.horaire_depart = horaire_depart;
-        this.horaire_arrivee = horaire_arrivee;
         this.num_ligne = num_ligne;
         this.listStation = listStation;
     }
--- src/main/java/Station.java
+++ src/main/java/Station.java
@@ -1,7 +1,11 @@
 public class Station {
     private String nom;
+    private String horaire_depart;
+    private String horaire_arrivee;
 
-    public Station(String nom) {
+    public Station(String nom, String horaire_depart, String horaire_arrivee) {
         this.nom = nom;
+        this.horaire_depart = horaire_depart;
+        this.horaire_arrivee = horaire_arrivee;
     }
 }
