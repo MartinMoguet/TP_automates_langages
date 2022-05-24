7978794687ff0701f747cae8d003c4f7c22573f4
MartinMoguet <martin.moguet@gmail.com>
2022-05-24 15:24:27 +0200

    changement classe station

--- src/main/java/Station.java
+++ src/main/java/Station.java
@@ -1,11 +1,9 @@
 public class Station {
     private String nom;
-    private Double horaire_depart;
-    private Double horaire_arrivee;
 
-    public Station(String nom, Double horaire_depart, Double horaire_arrivee) {
+
+    public Station(String nom) {
         this.nom = nom;
-        this.horaire_depart = horaire_depart;
-        this.horaire_arrivee = horaire_arrivee;
+
     }
 }
